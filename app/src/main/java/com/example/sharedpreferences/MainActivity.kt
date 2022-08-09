6ec32b6469c76bf33b811cf2ad2c5d3738702f16
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-09 20:42:52 -0300

    commit do intervalo

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.sharedpreferences
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSalvar.setOnClickListener(View.OnClickListener {
+            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+
+            val editor = saudacaoPersistencia.edit()
+
+            editor.putString("nome", txtNome.text.toString())
+            editor.putString("tratamento", listTratamento.selectedItem.toString())
+
+            Toast.makeText(this, "Salvo com Sucesso", Toast.LENGTH_SHORT).show()
+        })
+
+        btnExibir.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, SaudacaoActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
---
+++ app/src/main/java/com/example/sharedpreferences/SaudacaoActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sharedpreferences
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_saudacao.*
+
+class SaudacaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saudacao)
+
+        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+
+        val nome = saudacaoPersistencia.getString("nome", "")
+        val tratamento = saudacaoPersistencia.getString("tratamento", "")
+
+        if(tratamento.equals("Sem Tratamento")){
+            lbSaudacao.text = nome
+        }
+        else {
+            lbSaudacao.text = tratamento + " "+nome
+        }
+    }
+}
