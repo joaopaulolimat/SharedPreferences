3cbd19a9054e8a278c2f69de0a2ee0900486261e
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-09 21:54:58 -0300

    utilizar file writer e file reader para gravar localmente informações

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,18 +19,11 @@ class MainActivity : AppCompatActivity() {
         // adicionar listener de click no botao
         btnSalvar.setOnClickListener(View.OnClickListener {
 
-            // instancia um container para local storage chamado saudacao no modo privado
-            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+            // concatenar valores para guardar no file stream
+            val data = txtNome.text.toString() + ":" + listTratamento.selectedItem.toString()
 
-            // trazer um editor para modificar o shared preferences
-            val editor = saudacaoPersistencia.edit()
-
-            // putString guarda um valor (ex.: txtNome.text.toString()) na chave (ex.: "nome") definida
-            editor.putString("nome", txtNome.text.toString())
-            editor.putString("tratamento", listTratamento.selectedItem.toString())
-
-            // aplicar modificações no shared preferences
-            editor.apply()
+            //chama método gravaDadoArquivo
+            gravaDadoArquivo("saudacao", data)
 
             // toast para notificar sucesso no salvamento
             Toast.makeText(this, "Salvo com Sucesso", Toast.LENGTH_SHORT).show()
@@ -39,5 +35,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SaudacaoActivity::class.java)
             startActivity(intent)
         })
+
+
+    }
+
+    fun gravaDadoArquivo(fileName: String, data: String) {
+        try{
+            // openFileOutput método para guardar informações locais num arquivo dentro do contexto da aplicação
+            val fs = openFileOutput(fileName, Context.MODE_PRIVATE)
+            // escrever no arquivo
+            fs.write(data.toByteArray())
+            // fecha os streams e libera os recursos para escrever arquivos
+            fs.close()
+        }
+        catch (e: FileNotFoundException) { Log.i("gravaDadoArquivo", "FileNotFoundException")}
+        catch (e: IOException) { Log.i("gravaDadoArquivo", "IOException")}
     }
 }
--- app/src/main/java/com/example/sharedpreferences/SaudacaoActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/SaudacaoActivity.kt
@@ -4,18 +4,25 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_saudacao.*
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
 
 class SaudacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudacao)
 
-        // instancia um container para local storage chamado saudacao no modo privado
-        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+        // chama o método recuperaDadoArquivo
+        val data = recuperaDadoArquivo("saudacao")
 
-        // trazer valor da chave para uma constante, o segundo parâmetro é um valor default para evitar null pointer
-        val nome = saudacaoPersistencia.getString("nome", "")
-        val tratamento = saudacaoPersistencia.getString("tratamento", "")
+        // quebra a string através do delimitador definido, no caso é ":"
+        val tokenizer = StringTokenizer(data, ":")
+
+        // verifica se há tokens dentro do tokenizer, se sim retorna para constante
+        val nome = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem nome"
+        val tratamento = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem tratamento"
 
         if(tratamento.equals("Sem Tratamento")){
             // mostrar texto na label
@@ -23,6 +30,31 @@ class SaudacaoActivity : AppCompatActivity() {
         }
         else {
             lbSaudacao.text = tratamento + " "+nome
+        }
+    }
+    fun recuperaDadoArquivo(fileName: String): String {
+        return try{
+            // abre o arquivo (fileName) criado no contexto da aplicação
+            val fi = openFileInput(fileName)
+
+            // le o arquivo recuperado acima e retorna seu valor para constante
+            val data = fi.readBytes()
+
+            // fecha o stream para liberar recursos
+            fi.close()
+
+            // conversão do conteúdo para string
+            data.toString()
+
+            // define os encorders de texto
+            data.toString(Charset.defaultCharset())
+
+        // catches para evitar crash do app, o primeiro para caso o
+        // arquivo não seja encontrado e o segundo para problemas em abrir o arquivo
+        } catch (e: FileNotFoundException) {
+            ""
+        } catch (e: IOException) {
+            ""
         }
     }
 }
